a71bbdcf11f29819d0af8cc07a8cb7ce61d53620
rohith-35 <rohith.d.2024.cse@rajalakshmi.edu.in>
2026-07-16 11:23:24 +0530

    Resolve merge conflict

--- geotag/settings.py
+++ geotag/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'geotag',
         'USER': 'root',
-        'PASSWORD': 'kaaviya_shree_2507',
+        'PASSWORD': '240701435',
         'HOST': 'localhost',
         'PORT': '3306',
     }
